5668a12570ec00a9b76413c49c8366e18047a48d
Fabricio Leonard Leopoldino <fabricioleonard@gmail.com>
2021-11-05 09:50:35 -0300

    Group by search

--- backend/src/main/java/com/fabricioleonard/dashboardvendas/repository/SaleRepository.java
+++ backend/src/main/java/com/fabricioleonard/dashboardvendas/repository/SaleRepository.java
@@ -1,7 +1,20 @@
 package com.fabricioleonard.dashboardvendas.repository;
 
+import com.fabricioleonard.dashboardvendas.dto.SaleSuccessDTO;
+import com.fabricioleonard.dashboardvendas.dto.SaleSumDTO;
 import com.fabricioleonard.dashboardvendas.entity.Sale;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+    @Query("SELECT new com.fabricioleonard.dashboardvendas.dto.SaleSumDTO(obj.seller, SUM(obj.amount)) "
+           + "FROM Sale obj GROUP BY obj.seller")
+    List<SaleSumDTO> amountGroupedBySeller();
+
+    @Query("SELECT new com.fabricioleonard.dashboardvendas.dto.SaleSuccessDTO(obj.seller, SUM(obj.visited), SUM(obj.deals)) "
+            + "FROM Sale obj GROUP BY obj.seller")
+    List<SaleSuccessDTO> successGroupedBySeller();
 }
--- backend/src/main/java/com/fabricioleonard/dashboardvendas/service/SaleService.java
+++ backend/src/main/java/com/fabricioleonard/dashboardvendas/service/SaleService.java
@@ -1,6 +1,8 @@
 package com.fabricioleonard.dashboardvendas.service;
 
 import com.fabricioleonard.dashboardvendas.dto.SaleDTO;
+import com.fabricioleonard.dashboardvendas.dto.SaleSuccessDTO;
+import com.fabricioleonard.dashboardvendas.dto.SaleSumDTO;
 import com.fabricioleonard.dashboardvendas.entity.Sale;
 import com.fabricioleonard.dashboardvendas.repository.SaleRepository;
 import com.fabricioleonard.dashboardvendas.repository.SellerRepository;
@@ -9,6 +11,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class SaleService {
@@ -24,5 +28,14 @@ public class SaleService {
         sellerRepository.findAll();
         Page<Sale> result = repository.findAll(pageable);
         return result.map(x -> new SaleDTO(x));
+    }
+    @Transactional(readOnly = true)
+    public List<SaleSumDTO> amountGroupedBySeller(){
+        return repository.amountGroupedBySeller();
+    }
+
+    @Transactional(readOnly = true)
+    public List<SaleSuccessDTO> successGroupedBySeller(){
+        return repository.successGroupedBySeller();
     }
 }

7acfabf318427c5b26ffe43c88b7c1ab97b4c7f4
Fabricio Leonard Leopoldino <fabricioleonard@gmail.com>
2021-11-05 16:35:44 -0300

    Home, navigation

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,16 +1,18 @@
-import imgDsDark from 'assets/img/ds-dark.svg';
+import imgDsDark from "assets/img/ds-dark.svg";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
-    return (
-      <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadom-sm">
-        <div className="container">
-            <nav className="my-2 my-md-0 mr-md-3">
-                <img src={imgDsDark} alt="devSuperior" width="120"></img>
-            </nav>
-        </div>
+  return (
+    <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadom-sm">
+      <div className="container">
+        <nav className="my-2 my-md-0 mr-md-3">
+          <Link to="/">
+            <img src={imgDsDark} alt="devSuperior" width="120"></img>
+          </Link>
+        </nav>
       </div>
-    );
-  }
-  
-  export default NavBar;
-  
+    </div>
+  );
+};
+
+export default NavBar;
